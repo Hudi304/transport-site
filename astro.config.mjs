931cc4af077f8cd1fa87835fe9b217cc2c85a52a
Hudi304 <hudisteanu.ma@gmail.com>
2026-07-01 21:44:04 +0300

    implement cool design

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hudi304.github.io",
-  base: "/transport-site",
   vite: {
     plugins: [tailwindcss()],
   },
